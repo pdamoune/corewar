b2c012a3f4bc5b30e61de4070970ef17596d6095
Philippe DAMOUNE <pdamoune@e2r12p21.42.fr>
2017-08-17 17:40:10 +0200

    test merge

--- vm/srcs/tools/usage.c
+++ vm/srcs/tools/usage.c
@@ -6,16 +6,16 @@
 /*   By: pdamoune <pdamoune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/17 13:10:59 by pdamoune          #+#    #+#             */
-/*   Updated: 2017/08/17 16:11:33 by pdamoune         ###   ########.fr       */
+/*   Updated: 2017/08/17 17:39:52 by pdamoune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
+#include <stdio.h>
+
 int		usage(void)
 {
-	ft_printf("Usage : ./corewar [-d N -s N -v N | \
-	-b --stealth | -n --stealth] [-a] <champion1.cor> <...>\n");
-	ft_prf("%4c-a%8c: \n", 0, 0);
+	ft_printf("Usage : ./ [-d N -s N -v N |");
 	return (0);
 }
