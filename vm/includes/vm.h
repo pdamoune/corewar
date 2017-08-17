39d06027761f5956a51a8496ae7b197b33e572bb
Philippe DAMOUNE <pdamoune@e2r12p21.42.fr>
2017-08-17 16:41:58 +0200

    ajout des sujets en anglais et d un main simple et usage

---
+++ vm/includes/vm.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vm.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdamoune <pdamoune@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/17 13:13:41 by pdamoune          #+#    #+#             */
+/*   Updated: 2017/08/17 16:11:29 by pdamoune         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VM_H
+# define VM_H
+# include "libft.h"
+/*
+** Main functions.
+*/
+
+int		main(int argc, char **argv);
+int		ft_prf(char *format, ...);
+
+/*
+** Tools functions.
+*/
+
+int		usage(void);
+
+#endif
