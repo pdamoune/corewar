45be510845454393ef7dd8b180b5694bd41424bd
philippe damoune <philippedamoune@gmail.com>
2017-08-18 02:41:37 +0200

    push propre

--- vm/srcs/main.c
+++ vm/srcs/main.c
@@ -6,11 +6,12 @@
 /*   By: pdamoune <pdamoune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/17 13:10:56 by pdamoune          #+#    #+#             */
-/*   Updated: 2017/08/17 15:46:48 by pdamoune         ###   ########.fr       */
+/*   Updated: 2017/08/18 01:07:43 by philippedamoune  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
+#define DEF salut
 
 int		main(int argc, char **argv)
 {
