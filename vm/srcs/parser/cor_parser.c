f054c64ae899a2d0ab9b8160c646cd085da276bd
philippe damoune <philippedamoune@gmail.com>
2017-08-21 20:36:31 +0200

    test

--- vm/srcs/parser/cor_parser.c
+++ vm/srcs/parser/cor_parser.c
@@ -6,7 +6,7 @@
 /*   By: pdamoune <pdamoune@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/18 16:29:53 by pdamoune          #+#    #+#             */
-/*   Updated: 2017/08/18 16:51:05 by pdamoune         ###   ########.fr       */
+/*   Updated: 2017/08/21 18:47:37 by philippedamoune  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,17 @@ int		cor_parser(char *champion1)
 	char	line[65];
 	t_ld	*data;
 
+	data = NULL;
 	ft_bzero(&line, 65);
 	fd = open(champion1, O_RDONLY);
-	while (read(fd, line, 16))
-		ft_ld_pushback(&data, line);
-	while (data)
+	// int i = 0;
+	read(fd, line, 32);
+	int i = 0;
+	while (i < 32)
 	{
-		int i = 0;
-		while (i < 64)
-		{
-			ft_printf("%x ", ((long)(data->content))[i]);
-			i += 8;
-		}
-
-		// write(1, line, 64);
+		ft_printf("%.2x ", line[i++] & 0xff);
+	}
 		write(1, "\n", 1);
 		data = data->next;
-	}
 	return (0);
 }
