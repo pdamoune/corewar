acf2f4e9ddf3cfebbd33d5130eb1dd30e524537e
Cloe LEGOUBE <clegoube@e3r8p7.42.fr>
2017-10-16 23:56:25 +0200

    Mise a la norme de strsplit_asm.c

--- asm/srcs/LIB_ASM/strsplit_asm.c
+++ asm/srcs/LIB_ASM/strsplit_asm.c
@@ -6,31 +6,18 @@
 /*   By: tdebarge <tdebarge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/15 18:50:34 by tdebarge          #+#    #+#             */
-/*   Updated: 2017/10/16 11:48:53 by tdebarge         ###   ########.fr       */
+/*   Updated: 2017/10/16 23:55:05 by clegoube         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/op.h"
 
-char			**ft_keep_first(char **tab2)
+static char			**ft_copy_tab_2(char **tab3, char **tab2)
 {
 	int		i;
 	int		j;
 	int		k;
-	int		len;
-	char	**tab3;
 
-	if (!tab2)
-		return (NULL);
-	i = 1;
-	len = 0;
-	while (tab2[i])
-		len += ft_strlen(tab2[i++]);
-	if (!(tab3 = (char**)malloc(sizeof(*tab3) * (2 + 1))))
-		return (NULL);
-	tab3[0] = ft_strdup(tab2[0]);
-	if (!(tab3[1] = (char*)malloc(sizeof(char) * (len + 1))))
-		return (NULL);
 	i = 1;
 	j = 0;
 	while (tab2[i])
@@ -46,13 +33,51 @@ char			**ft_keep_first(char **tab2)
 	return (tab3);
 }
 
+
+static char			**ft_copy_tab_1_and_3(char **tab3, char **tab1, char **tab)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (tab3[j])
+		tab[i++] = ft_strdup(tab3[j++]);
+	j = 1;
+	while (tab1[j])
+		tab[i++] = ft_strdup(tab1[j++]);
+	tab[i] = NULL;
+	ft_free_tab(tab1);
+	ft_free_tab(tab3);
+	return (tab);
+}
+
+char			**ft_keep_first(char **tab2)
+{
+	int		i;
+	int		len;
+	char	**tab3;
+
+	if (!tab2)
+		return (NULL);
+	i = 1;
+	len = 0;
+	while (tab2[i])
+		len += ft_strlen(tab2[i++]);
+	if (!(tab3 = (char**)malloc(sizeof(*tab3) * (2 + 1))))
+		return (NULL);
+	tab3[0] = ft_strdup(tab2[0]);
+	if (!(tab3[1] = (char*)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	tab3 = ft_copy_tab_2(tab3, tab2);
+	return (tab3);
+}
+
 char			**ft_split_tab(char **tab1)
 {
 	char	**tab2;
 	char	**tab3;
 	char	**tab;
-	int		i;
-	int		j;
 	int		len;
 
 	if (!tab1)
@@ -66,15 +91,6 @@ char			**ft_split_tab(char **tab1)
 	len = ft_strlen_tab(tab3) + ft_strlen_tab(tab1);
 	if (!(tab = (char**)malloc(sizeof(*tab) * (len + 1))))
 		return (NULL);
-	i = 0;
-	j = 0;
-	while (tab3[j])
-		tab[i++] = ft_strdup(tab3[j++]);
-	j = 1;
-	while (tab1[j])
-		tab[i++] = ft_strdup(tab1[j++]);
-	tab[i] = NULL;
-	ft_free_tab(tab1);
-	ft_free_tab(tab3);
+	tab = ft_copy_tab_1_and_3(tab3, tab1, tab);
 	return (tab);
 }
