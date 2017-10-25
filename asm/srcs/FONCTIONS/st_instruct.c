a7b3271561d184d02067c4cde6bf749afd627300
wescande <wescande@student.42.fr>
2017-10-25 21:49:51 +0200

    solve merge

--- asm/srcs/FONCTIONS/st_instruct.c
+++
@@ -1,9 +0,0 @@
-#include "../includes/op.h"
-
-
-void	st_instruct(global_t *global, int step)
-{
-	printf("<%s> = 0x03 \n", global->s_label->s_content->line[0]);
-	ft_print_words_tables(global->s_label->s_content->line);
-	step = 0;
-}
